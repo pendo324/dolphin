763755045134709354305d4d6d7cbc17adfca095
pendo324 <pendo3247@gmail.com>
2015-04-04 00:08:09 -0400

    Started adding netcode stuff

--- Source/Core/DolphinWX/Updater.cpp
+++ Source/Core/DolphinWX/Updater.cpp
@@ -1,13 +1,54 @@
-#include "Common/MsgHandler.h"
 #include <SFML/Network/Http.hpp>
+
+#include "Common/MsgHandler.h"
 #include "Updater.h"
+#include "Common\Common.h"
+#include "DolphinWX/WxUtils.h"
+
 
 void CUpdater::CheckUpdate()
 {
-	CUpdater::Update();
+	int m_newBranch = 0;
+	int m_newGit = 0;
+	std::string versionAndRevision;
+	std::string newVersion;
+
+	memcpy(&versionAndRevision, scm_desc_str, strlen(scm_desc_str) - 6);
+
+	//net shit
+	sf::Http::Request req;
+	req.setUri("/json/builders/release-win-x64/builds/-1/");
+
+	sf::Http http;
+	http.setHost("buildbot.dolphin-emu.org");
+
+	const sf::Http::Response resp = http.sendRequest(req, sf::seconds(5));
+
+	if (sf::Http::Response::Ok == resp.getStatus())
+	{
+		//parse through the response body, gl m8
+		std::istringstream ss(resp.getBody);
+	}
+	else
+	{
+		WxUtils::ShowErrorDialog("Failed to download.");
+	}
+
+
+	//get the version from the json file and assign it to newVersion format: version-revision
+
+	if (strcmp(versionAndRevision.c_str(), newVersion.c_str()) != 0) {
+		if (AskYesNo("Current version: %s\nNew version: %s\n", versionAndRevision, newVersion) == 1) 
+		{
+			//if they press yes, update
+			CUpdater::Update();
+		}
+	}
+
+
 }
 
 void CUpdater::Update()
 {
-	SuccessAlertT("Dolphin will update upon application close");
+	SuccessAlertT("Doing updater things...");
 }
